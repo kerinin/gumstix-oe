2f8b3a253b0f9d0b254ab4eaac522878ba6d72ec
sakoman <sakoman@b85c6266-aee1-4070-9685-74336958316a>
2007-12-08 07:49:15 +0000

    Further autobuilder tweaks
    git-svn-id: https://gumstix.svn.sourceforge.net/svnroot/gumstix/trunk@35 b85c6266-aee1-4070-9685-74336958316a

--- extras/autobuilder.sh
+++ extras/autobuilder.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
 # edit line below for your setup
-export GUMSTIXTOP="/home/oe/gumstix/gumstix-oe"
+GUMSTIXTOP="/home/oe/gumstix/gumstix-oe"
 
-# path to feed archives directory
-export OE_FEED="/var/www/feeds/archives"
+# path to feed directory
+OE_FEED="/var/www/feeds"
+
+# list of machines to build for
+BUILD_MACHINES=" \
+                gumstix-custom-verdex \
+                gumstix-custom-connex \
+               "
+
+# list of build targets
+BUILD_TARGETS=" \
+               gumstix-minimal-image \
+               gumsix-basic-image \
+               gumstix-directfb-image \
+              "
 
 cd $GUMSTIXTOP
 
@@ -12,24 +25,29 @@ cd $GUMSTIXTOP
 
 svn update
 
-export REVISION=`svnversion`
+REVISION=`svnversion`
 
-if [ ! -e $OE_FEED/$REVISION ];
+if [ ! -e $OE_FEED/archive/$REVISION ];
 then
   echo "Building revision $REVISION"
 
   rm -rf tmp
 
-  for i in gumstix-custom-connex gumstix-custom-verdex
+  for machine in $BUILD_MACHINES
+  do
+
+    echo "MACHINE = \"$machine\"" > build/conf/auto.conf
+
+    for target in $BUILD_TARGETS
     do
-      echo "MACHINE = \"$i\"" > build/conf/auto.conf
-      bitbake gumstix-basic-image
-      bitbake gumstix-directfb-image
+      bitbake $target
     done
 
-  mkdir -p $OE_FEED/$REVISION
-  cp -rf tmp/deploy/* $OE_FEED/$REVISION
+  done
 
+  mkdir -p $OE_FEED/archive/$REVISION
+  cp -rf tmp/deploy/* $OE_FEED/archive/$REVISION
+  
   svn revert build/conf/auto.conf
 fi
 
