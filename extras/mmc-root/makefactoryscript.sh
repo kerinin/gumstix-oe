80c325792cd26721d5504ef9d12079fca7886c27
sakoman <sakoman@b85c6266-aee1-4070-9685-74336958316a>
2008-02-27 06:09:33 +0000

    Check in some preliminary work needed for booting from flash cards
    git-svn-id: https://gumstix.svn.sourceforge.net/svnroot/gumstix/trunk@251 b85c6266-aee1-4070-9685-74336958316a

---
+++ extras/mmc-root/makefactoryscript.sh
@@ -0,0 +1,1 @@
+~/gumstix/gumstix-buildroot/build_arm_nofpu/staging_dir/bin/mkimage  -A arm -O linux -T script -C none -a 0 -e 0 -n gumstix-factory.script -d gumstix-factory.script.source gumstix-factory.script
